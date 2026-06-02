66cb368bf339086d7e0d967258bfa771e12a13c5
muhammadfaisal87677-beep <muhammadfaisal87677@icloud.com>
2026-06-02 22:37:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,82 @@
 const tombol = document.getElementById("btnKenangan");
 
 tombol.addEventListener("click", function () {
-
     document.getElementById("kenangan").style.display = "block";
-
 });
 
-const tanggalJadian = new Date("2025-06-01");
+const tanggalJadian = new Date("2025-06-01T00:00:00");
 
-const hariIni = new Date();
+function updateCounter() {
 
-const selisihWaktu = hariIni - tanggalJadian;
+    const sekarang = new Date();
 
-const jumlahHari = Math.floor(
-    selisihWaktu / (1000 * 60 * 60 * 24)
-);
+    const selisih = sekarang - tanggalJadian;
 
-document.getElementById("hari").innerHTML =
-    "❤️ Kita telah bersama selama <b>" +
-    jumlahHari +
-    " hari</b> ❤️";
+    const hari = Math.floor(
+        selisih / (1000 * 60 * 60 * 24)
+    );
+
+    const jam = Math.floor(
+        (selisih % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const menit = Math.floor(
+        (selisih % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const detik = Math.floor(
+        (selisih % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("hari").innerHTML = `
+
+        <h3>❤️ Kita Telah Bersama ❤️</h3>
+
+        <div class="hari-box">
+            ${hari}
+        </div>
+
+        <div class="hari-label">
+            Hari
+        </div>
+
+        <div class="time-container">
+
+            <div class="time-box">
+                <div class="angka">
+                    ${jam.toString().padStart(2, '0')}
+                </div>
+                <div class="label">
+                    Jam
+                </div>
+            </div>
+
+            <div class="time-box">
+                <div class="angka">
+                    ${menit.toString().padStart(2, '0')}
+                </div>
+                <div class="label">
+                    Menit
+                </div>
+            </div>
+
+            <div class="time-box">
+                <div class="angka">
+                    ${detik.toString().padStart(2, '0')}
+                </div>
+                <div class="label">
+                    Detik
+                </div>
+            </div>
+
+        </div>
+
+    `;
+}
+
+updateCounter();
+
+setInterval(updateCounter, 1000);
